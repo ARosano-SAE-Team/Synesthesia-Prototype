66716db658d2c23ed184b037dd294e299a917c62
Alex-Rosano-SAE <60865530+Alex-Rosano-SAE@users.noreply.github.com>
2020-03-22 20:54:49 +1100

    Add Simulation project

---
+++ Assets/_Scripts/ParamCube.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using _Scripts;
+using UnityEngine;
+
+public class ParamCube : MonoBehaviour
+{
+    public int _band;
+    public float _startScale, _scaleMultiplier;
+    public bool _useBuffer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_useBuffer)
+        {
+            transform.localScale = new Vector3 (transform.localScale.x, (Audioscript._bandBuffer [_band] * _scaleMultiplier) + _startScale, transform.localScale.z);
+        }
+        if (!_useBuffer)
+        {
+            transform.localScale = new Vector3 (transform.localScale.x, (Audioscript._freqBand [_band] * _scaleMultiplier) + _startScale, transform.localScale.z);
+        }
+    }
+}
